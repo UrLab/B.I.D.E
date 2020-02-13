648c23d5ba4cfbbf2454dbf43877c04adbbee288
fgrimau <florent.grimau@ulb.be>
2020-02-13 18:47:28 +0100

    [Add] jokes modules

--- bide.py
+++ bide.py
@@ -25,10 +25,16 @@ def jokes():
     co = sqlite3.connect('users.db')
     c = co.cursor()
 
+    i = 1
+    jokes = []
+    for vals in c.execute("SELECT * FROM jokes ORDER BY points DESC"):
+        jokes.append(((vals[0], vals[1], vals[2]), i))
+        i += 1
+
     # Save (commit) the changes
     co.close()
 
-    return render_template('jokes.html')
+    return render_template('jokes.html', jokes=jokes)
 
 
 @app.route("/adduser", methods=['POST'])
@@ -36,7 +42,6 @@ def insert_user():
     username = request.form.get("Name")
     if username == "":
         return redirect("/", code=302)
-    print(type(username))
 
     # Connecting to the database and inserting the username
     co = sqlite3.connect("users.db")
@@ -46,6 +51,51 @@ def insert_user():
     co.close()
 
     return redirect("/", code=302)
+
+
+@app.route("/addjoke", methods=['POST'])
+def insert_joke():
+    joke = request.form.get("Joke")
+    if joke == "":
+        return redirect("/", code=302)
+
+    co = sqlite3.connect("users.db")
+    c = co.cursor()
+
+    pk = int(c.execute("SELECT MAX(pk) FROM jokes").fetchall()[0][0])
+    c.execute("INSERT INTO jokes VALUES (?, 0, ?)", (joke, pk+1))
+
+    co.commit()
+    co.close()
+
+    return redirect("/jokes", code=302)
+
+
+@app.route("/vote", methods=['GET'])
+def vote_joke():
+    joke_pk = request.args.get("joke")
+    action = request.args.get("action")
+    if joke_pk == "":
+        return redirect("/jokes", code=302)
+
+    co = sqlite3.connect("users.db")
+    c = co.cursor()
+
+    base_points = c.execute("SELECT points FROM jokes WHERE pk=?", (joke_pk,))
+    if action == "plus":
+        points = int(base_points.fetchall()[0][0]+1)
+    elif action == "moins":
+        points = int(base_points.fetchall()[0][0]-1)
+    else:
+        c.close()
+        return redirect("/jokes", code=302)
+
+    c.execute("UPDATE jokes SET points=? WHERE pk=?", (points, joke_pk))
+
+    co.commit()
+    co.close()
+
+    return redirect("/jokes", code=302)
 
 
 @app.route("/ModPoints", methods=['GET'])
